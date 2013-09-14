cec8fad6eae33687d36c195d143266cf13185f13
Ed Kent <ed.kent@gmail.com>
2013-09-14 22:00:56 +0100

    Fix spot price history so it appears and disappears as it should

--- cloud_copasi/web_interface/templates/pool/pool_launch.js
+++ cloud_copasi/web_interface/templates/pool/pool_launch.js
@@ -19,7 +19,12 @@ function showSpotPriceField(){
 
 function showSpotPrice(){
     //Insert a field after bottom_line
-    history_line = '<tr><td class="topline" colspan="2"> </td></tr><tr class="formrow" id="spotprice-row"><th class="fieldlabel required">Spot price:</th><td id="spotprice"><div id="spotprice_current">Loading current price...</div></td><tr><td colspan="2"><div id="spotprice_history" style="width:400px; height:300px"></div></td></tr><tr><td class="bottomline" colspan="2"> </td></tr>';
+    history_line = '<tr><td class="topline" colspan="2"> </td></tr>\
+    <tr class="formrow" id="spotprice-row"><th class="fieldlabel required">Current spot price:</th>\
+        <td id="spotprice"><div id="spotprice_current">Loading current price...</div></td>\
+    </tr>\
+    <tr id="spotprice_history_row" style="height:300px"><th class="fieldlabel required" style="vertical-align:middle;">Spot price history:</th><td><div id="spotprice_history" style="width:600px; height:300px"></div></td></tr>\
+    <tr><td class="bottomline" colspan="2"> </td></tr>';
     bottom_line.after(history_line);
     
     $('#spotprice_current').fadeIn();
@@ -29,11 +34,11 @@ function updateSpotPrice(){
     var instance_type = $('#id_initial_instance_type').val();
     var spotprice_current = $('#spotprice_current');
     spotprice_current.hide();
-    spotprice_current.html('Loading...')
+    spotprice_current.html('Loading current price...')
     spotprice_current.fadeIn();
     $.getJSON("{% url 'api_spot_price' %}", {'key_id': "{{key_id}}", 'instance_type':instance_type }, function(data){
         spotprice_current.hide();
-        spotprice_current.html('$' + data['price']);
+        spotprice_current.html('$' + data['price'] + '  (' + instance_type + ')');
         spotprice_current.fadeIn();
     });
     
@@ -41,20 +46,28 @@ function updateSpotPrice(){
     {
         updateSpotPriceHistory();
     }
+    else
+    {
+        $('#spotprice_history_row').hide()
+    }
     
 }
 
 function updateSpotPriceHistory(){
     var instance_type = $('#id_initial_instance_type').val();
-    //$('#spotprice_history').fadeOut();
+    $('#spotprice_history_row').fadeOut();
+    $('#spotprice_history').html('');
+    $('#spotprice_history').html('<div style="height:300px; line-height:300px"><span style="line-height:normal; vertical-align: middle">Loading price history...</span></div>');
+    $('#spotprice_history_row').fadeIn();
+
     $.getJSON("{% url 'api_spot_price' %}", {'key_id': "{{key_id}}", 'instance_type':instance_type, 'history':true}, function(data){
         price = data['price'];
 
         function drawChart() {
             //var data = google.visualization.arrayToDataTable(price);
             var data = new google.visualization.DataTable();
-            data.addColumn('date', 'Date');
-            data.addColumn('number', 'Price');
+            data.addColumn('datetime', 'Date');
+            data.addColumn('number', 'Price $');
 
             for (var i=0; i<price.length; i++)
             {
@@ -64,6 +77,9 @@ function updateSpotPriceHistory(){
             }
             var options = {
                 title: 'Price history',
+                displayExactValues: true,
+                displayZoomButtons: false,
+                
                 
             };
 
@@ -93,7 +109,7 @@ function windowLoad(){
     }
     $('#id_pricing_0').click(function(){ //fixed price selected
         hideSpotPriceField(true);
-        $('#spotprice_history').fadeOut();
+        $('#spotprice_history_row').fadeOut();
 
     });
   
