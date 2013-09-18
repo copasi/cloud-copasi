5e84465744d2c9b52377819943b09e2796a49981
Ed Kent <ed.kent@gmail.com>
2013-09-18 18:29:53 +0100

    Create package for email notifications

---
+++ cloud_copasi/web_interface/email/email_tools.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2013 Edward Kent.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the GNU Public License v3.0
+# which accompanies this distribution, and is available at
+# http://www.gnu.org/licenses/gpl.html
+# 
+# Contributors:
+#     Edward Kent - initial API and implementation
+#-------------------------------------------------------------------------------
+# Django settings for cloud_copasi project.
+
+
