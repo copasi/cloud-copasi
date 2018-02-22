262b1fbd23bbd210d0a297bd60090b7004b5081d
Brian Klahn <briandk@vt.edu>
2018-02-22 17:05:17 -0500

    Automatically start bosco.

--- cloud-copasi-daemon.sh
+++ cloud-copasi-daemon.sh
@@ -7,4 +7,8 @@ export PYTHONPATH=$PYTHONPATH:/home/cloudcopasi/cloud-copasi
 export PYTHONHOME=/home/cloudcopasi/cloud-copasi/venv
 export DJANGO_SETTINGS_MODULE=cloud_copasi.settings
 
+# First start bosco
+source /home/cloudcopasi/bosco/bosco_setenv
+bosco_start
+
 python /home/cloudcopasi/cloud-copasi/cloud_copasi/background_daemon/cloud_copasi_daemon.py $@
