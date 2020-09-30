fa43f70ed77fed7bdec698618fe075c87446b356
Hasan Baig <hasan.baig@hotmail.com>
2020-09-30 13:23:29 -0400

    server cloud-copasi-new started for the first time

--- cloud_copasi/cloud_copasi/wsgi.py
+++ cloud_copasi/cloud_copasi/wsgi.py
@@ -1,16 +1,42 @@
+#-------------------------------------------------------------------------------
+# Cloud-COPASI
+# Copyright (c) 2013 Edward Kent.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the GNU Public License v3.0
+# which accompanies this distribution, and is available at
+# http://www.gnu.org/licenses/gpl.html
+#-------------------------------------------------------------------------------
 """
 WSGI config for cloud_copasi project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+This module contains the WSGI application used by Django's development server
+and any production WSGI deployments. It should expose a module-level variable
+named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
+this application via the ``WSGI_APPLICATION`` setting.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+Usually you will have the standard Django WSGI application here, but it also
+might make sense to replace the whole Django WSGI application with a custom one
+that later delegates to the Django one. For example, you could introduce WSGI
+middleware here, or combine a Django application with an application of another
+framework.
+
 """
+import os, sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
-import os
+#following two lines are added by HB
+sys.path.append('home/cloudcopasi/cloud-copasi-new/cloud_copasi')
+sys.path.append('/home/cloudcopasi/cloud-copasi-new/ccEnv/lib/python3.6/site-packages')
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cloud_copasi.settings")
+
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cloud_copasi.settings')
-
 application = get_wsgi_application()
+
+# Apply WSGI middleware here.
+# from helloworld.wsgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
