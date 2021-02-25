f5a63327d195ccbd158e19bf9d67772c7b000825
root <root@cloud-copasi-new.cam.uchc.edu>
2021-02-25 02:22:28 -0500

    files corrected

--- cloud_copasi/cloud_copasi/wsgi.py
+++ cloud_copasi/cloud_copasi/wsgi.py
@@ -26,10 +26,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../.
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
 #following two lines are added by HB
-sys.path.append('home/cloudcopasi/cloud-copasi/cloud_copasi')
+sys.path.append('/home/cloudcopasi/cloud-copasi/cloud_copasi')
 sys.path.append('/home/cloudcopasi/cloud-copasi/venv/lib/python3.8/site-packages')
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cloud_copasi.settings")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cloud_copasi.settings")
 
