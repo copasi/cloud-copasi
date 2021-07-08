62b7786707a751c78d0652e2c5d83c852b47c7c2
root <root@cloud-copasi-new.cam.uchc.edu>
2021-07-08 14:35:33 -0400

    statements in background script added for debugging

--- cloud_copasi/background_daemon/tools/background_script.py
+++ cloud_copasi/background_daemon/tools/background_script.py
@@ -22,29 +22,45 @@ import logging
 from cloud_copasi.web_interface.pools import condor_tools, task_tools
 from cloud_copasi.background_daemon.tools import pool_tools
 
+########### following lines are set by HB for debugging
+logging.basicConfig(
+        filename='/home/cloudcopasi/log/debug.log',
+        format='%(asctime)s %(levelname)s: %(message)s',
+        datefmt='%m/%d/%y %I:%M:%S %p',
+        level=logging.DEBUG
+    )
+######################################################
+
+
 if __name__ == '__main__':
-    log = logging.getLogger('cloud_copasi.background_daemon.tools.background_script')
+    #log = logging.getLogger('cloud_copasi.background_daemon.tools.background_script')
+    check = logging.getLogger('cloud_copasi.background_daemon.tools.background_script')   #added by HB
 else:
-    log = logging.getLogger(__name__)
+    #log = logging.getLogger(__name__)
+    check = logging.getLogger(__name__)   #added by HB
 
 
 
 def run():
+    check.debug("_________________ run() function runs _________________")
     try:
         pool_tools.refresh_all_ec2_pools()
     except Exception as e:
         log.exception(e)
     
-    log.debug('@@@@@ running process_condor_q $$$$$$$$$$')
+    check.debug('@@@@@ running process_condor_q by background_script $$$$$$$$$$')
     condor_tools.process_condor_q()
-
+    
+    check.debug('@@@@@ running update_tasks() by background_script $$$$$$$$$$')
     task_tools.update_tasks()
 
     try:
         pool_tools.terminate_idle_pools()
     except Exception as e:
-        log.exception(e)
+        #log.exception(e)
+        check.exception(e)   #added by HB
 
-    log.debug('Finished background script')
+    check.debug('Finished background script')
+
 if __name__ == '__main__':
     run()
