f9804d182c8329c17822b890f43ec88e0e990b13
root <root@cloud-copasi-new.cam.uchc.edu>
2021-07-09 23:03:24 -0400

    after restarting server to reflect the changes

--- cloud_copasi/web_interface/pools/condor_tools.py
+++ cloud_copasi/web_interface/pools/condor_tools.py
@@ -410,14 +410,17 @@ def read_condor_q():
     #check.debug(condor_q_output_str)
 
     if no_of_jobs > 0:
+        check.debug("_*_*_*_*_ if block executed _*_*_*_*_")
         job_string = r'\s*(?P<cluster_id>\d+)\.(?P<process_id>\d+)\s+(?P<owner>\S+)\s+(?P<sub_date>\S+)\s+(?P<sub_time>\S+)\s+(?P<run_time>\S+)\s+(?P<status>\w)\s+(?P<pri>\d+)\s+(?P<size>\S+)\s+(?P<cmd>\S+)'
         job_re = re.compile(job_string)
         #added by HB. following for-loop command is modified to process the condor_q_output in string format.
         #for job_listing in condor_q_output_str:
         for job_listing in condor_q_output:
+            check.debug(" ******* job_listing: %s" %job_listing)
             match = job_re.match(job_listing)
+            check.debug(" ******* match: %s" %match)    #added by HB
+    
             if match:
-                check.debug(" ******* match: %s" %match)    #added by HB
                 cluster_id = int(match.group('cluster_id'))
                 check.debug(" ******* cluster_id: %d" %cluster_id)  #added by HB
                 process_id = int(match.group('process_id'))
