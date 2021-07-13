56dc63976c07fc1d7db14c23e0e091d1ab847945
Hasan Baig <hasan.baig@hotmail.com>
2021-07-13 12:52:15 -0400

    debugging condor_log_files for reading log strings

--- cloud_copasi/web_interface/pools/condor_log_tools.py
+++ cloud_copasi/web_interface/pools/condor_log_tools.py
@@ -12,18 +12,18 @@ import re, datetime
 
 class Log:
     """Class for reading and processing condor log files using regex patterns"""
-   
+
     def __init__(self, path):
         """Initialise the class, reading from the condor file located at absolute path 'path'"""
-       
+
         #First, define some regexes
-       
+
         #Job execution string will be of the format:
         #001 (20949.000.000) 02/07 11:27:10 Job executing on host: <130.88.110.118:60608>
         execution_string = r'\d+\s\S+\s(?P<month>\d\d)\/(?P<day>\d\d)\s(?P<hour>\d+)\:(?P<minute>\d+)\:(?P<second>\d+)\sJob executing on host\:\s\<(?P<host>.+)\>.*'
         execution_re = re.compile(execution_string)
         execution_match = False
-        
+
         #Job termination string:
         #005 (20949.000.000) 02/07 11:28:10 Job terminated.
         termination_string = r'\d+\s\S+\s(?P<month>\d\d)\/(?P<day>\d\d)\s(?P<hour>\d+)\:(?P<minute>\d+)\:(?P<second>\d+)\sJob terminated\..*'
@@ -42,14 +42,18 @@ class Log:
         #We'll use this string to search for the phrase 'Job terminated'. We'll then use this to decide whether or not the job has finished running yet. If not, then don't try and match anything else yet - there's no point
         termination_confirmation_string = r'.*Job terminated.'
         termination_confirmation_re = re.compile(termination_confirmation_string)
-        
+
         self.has_terminated = False
         #Search to see if the job has actually terminated. Only continue if it has...
         for line in open(path, 'r'):
+            check.debug("********* (condo_log_tools) Checking Log line: ")
+            check.debug(line)
             if termination_confirmation_re.match(line):
                 self.has_terminated = True
+                check.debug("********* (condo_log_tools) Job terminated....exiting. ")
                 break
-       
+
+        check.debug("********* (condo_log_tools) Not terminated yet. ")
         if not self.has_terminated:
             return
 
@@ -57,14 +61,18 @@ class Log:
             for line in open(path, 'r'):
                 if execution_re.match(line):
                     execution_match = execution_re.match(line)
+                    check.debug("**** execution_match")
                 elif termination_re.match(line):
                     termination_match = termination_re.match(line)
+                    check.debug("**** termination_match")
                 elif termination_status_re.match(line):
                     termination_status_match = termination_status_re.match(line)
+                    check.debug("**** termination_status_match")
                 elif remote_usage_re.match(line):
                     remote_usage_match = remote_usage_re.match(line)
-               
-           
+                    check.debug("**** remote_usage_match")
+
+
             if remote_usage_match:
                 g = remote_usage_match.group
                 usr_days = int(g('usr_days'))
@@ -76,12 +84,12 @@ class Log:
                 sys_hours = int(g('sys_hours'))
                 sys_minutes = int(g('sys_minutes'))
                 sys_seconds = int(g('sys_seconds'))
-               
+
                 usr_time = datetime.timedelta(days=usr_days, hours=usr_hours, minutes=usr_minutes, seconds=usr_seconds)
                 sys_time = datetime.timedelta(days=sys_days, hours=sys_hours, minutes=sys_minutes, seconds=sys_seconds)
-               
+
                 self.remote_usage_time = usr_time + sys_time
-           
+
             if execution_match:
                 g = execution_match.group
                 day = int(g('day'))
@@ -91,7 +99,7 @@ class Log:
                 second = int(g('second'))
                 host = g('host')
 #                port = g('port')
-               
+
                 #Create datetime object for execution start time
                 #Since log file doesn't store the date, we'll have to guess it
                 #If remote_usage time is stored, take the current date, and subtract the usage time
@@ -100,13 +108,13 @@ class Log:
                     year = (datetime.datetime.today() - self.remote_usage_time).year
                 except AssertionError:
                     year = datetime.datetime.today().year
-               
+
                 self.execution_start = datetime.datetime(year=year, month=month, day=day, hour=hour, minute=minute, second=second)
                 self.host = host
-               
+
             if termination_status_match:
                 self.termination_status = int(termination_status_match.group('return_value'))
-               
+
             if termination_match:
                 g = termination_match.group
                 day = int(g('day'))
@@ -114,19 +122,18 @@ class Log:
                 hour = int(g('hour'))
                 minute = int(g('minute'))
                 second = int(g('second'))
-               
+
                 #Again, guess the year
                 year=datetime.datetime.today().year
                 self.termination_time = datetime.datetime(year=year, month=month, day=day, hour=hour, minute=minute, second=second)
 
             #For some reason, the remote usage time sometimes appears as zero. In this case, set running time as follows:
-            if self.remote_usage_time == datetime.timedelta() and hasattr(self, 'execution_start'):            
+            if self.remote_usage_time == datetime.timedelta() and hasattr(self, 'execution_start'):
                 self.running_time = self.termination_time - self.execution_start
             else:
                 self.running_time = self.remote_usage_time
             self.running_time_in_days = float(self.running_time.days) + (float(self.running_time.seconds) / 86400.00)
-             
-               
+
+
         except:
             raise
-
