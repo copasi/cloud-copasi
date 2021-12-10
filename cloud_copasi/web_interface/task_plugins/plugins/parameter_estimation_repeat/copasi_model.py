0b60c78d8c2f2094764567c19e78bce520684a99
Hasan Baig <hasan.baig@hotmail.com>
2021-12-09 21:14:51 -0500

    copasi_model.py file is updated with basiCO class.

--- cloud_copasi/web_interface/task_plugins/plugins/parameter_estimation_repeat/copasi_model.py
+++ cloud_copasi/web_interface/task_plugins/plugins/parameter_estimation_repeat/copasi_model.py
@@ -130,3 +130,46 @@ class PRCopasiModel(CopasiModel):
 
 
         return ['load_balancing_%d.cps' % repeat for repeat in repeats]
+
+class PRCopasiModel_BasiCO(CopasiModelBasiCO):
+
+    def prepare_pr_load_balancing(self, repeats=None):
+
+        if not repeats:
+            repeats = [1, 10, 100, 1000]
+
+        self._clear_tasks()
+        fitTask = get_fit_parameters()
+
+        report_key = None
+        self._create_report('PR', report_key, 'auto_pr_report')
+
+        self.scan_items[0]['num_steps'] = 0
+        self.scan_items[0]['type'] = 'repeat'
+
+
+        set_scan_settings(
+                          scheduled = True,
+                          update_model = True,
+                          cn='',
+                          subtask = 'Parameter Estimation',
+                          output_during_subtask = True,
+                          adjust_initial_conditions = False,
+                          scan_items = self.scan_items,
+                        )
+
+
+        assign_report('auto_pr_report', task=T.SCAN, append=True)
+        assign_report('auto_pr_report', task=T.PARAMETER_ESTIMATION, append=True)
+
+        for repeat in repeats:
+            # filename = os.path.join(self.path, 'load_balancing_%d.cps' %repeat) #for production
+            filename = os.path.join(os.getcwd(), 'load_balancing_%d.cps' %repeat) #for pythonHelp
+            target = str(repeat) + '_out.txt'
+            assign_report('auto_pr_report', task=T.SCAN, filename=target, append=True, confirm_overwrite = False)
+            self.scan_items[0]['num_steps'] = repeat
+            set_scan_items(self.scan_items)
+            self.write(filename)
+
+        return ['load_balancing_%d.cps' % repeat for repeat in repeats]
+        
