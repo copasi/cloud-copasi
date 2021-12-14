34d4634140fd51afdc6b8e14d47283d1771c6e6d
Hasan Baig <hasan.baig@hotmail.com>
2021-12-14 13:25:08 -0500

    librarie order changed

--- cloud_copasi/web_interface/task_plugins/plugins/parameter_estimation_repeat/copasi_model.py
+++ cloud_copasi/web_interface/task_plugins/plugins/parameter_estimation_repeat/copasi_model.py
@@ -7,8 +7,8 @@
 # http://www.gnu.org/licenses/gpl.html
 #-------------------------------------------------------------------------------
 from basico import *
-from cloud_copasi.copasi.model import *
 from cloud_copasi.copasi import model
+from cloud_copasi.copasi.model import *
 from cloud_copasi import settings
 from lxml import etree
 import os, time, math
