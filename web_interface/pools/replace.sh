9d465304d020adcefd0f3d3aae21e86372fca1d8
Pedro Mendes <mendes@copasi.org>
2022-04-05 11:20:59 -0400

    instead of editing slurm_submit.sh we now push slurm_local_submit_attributes.sh

--- web_interface/pools/replace.sh
+++ web_interface/pools/replace.sh
@@ -1,19 +1,49 @@
 #!/bin/bash
-# Hasan Baig
+# replace.sh - replace slurm_local_submit_attributes.sh on a slurm submit node
+# usage: replace.sh partition qos remote_host
+# remote_host needs to be the usual bosco pool address: user@submit.node.edu
+#
+# Written by Hasan Baig and Pedro Mendes, 2021-2022
 
-SAMPLE_FILENAME='sample_slurm_submit.sh'
-FILE_TO_TRANSFER='slurm_submit.sh'
+# currently, condor version is not needed, uncomment here and if/else below if needed
+# get the condor version, inlcuding major and minor (3rd level not needed as of now)
+#VERSION=`~/condor/bin/condor_version | head -n 1 | sed 's/.*\([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/'`
+#MAJOR=$(expr match "$VERSION" '^\([0-9]\+\).*')
+#MINOR=$(expr match "$VERSION" '^[0-9]\+\.\([0-9]\+\).*')
 
-ORIGINAL_PSTRING='PPPPPPPP'
-NEW_PSTRING=$1
+# temporary file to hold the slurm partition and qos, will be transferred to submit node
+FILETMP="/tmp/slurm_local_submit_attributes.$RANDOM"
 
-ORIGINAL_QSTRING='QQQQQQQQ'
-NEW_QSTRING=$2
+# add SBATCH commands to file
+echo "#!/bin/bash" > $FILETMP
+echo "echo \"#SBATCH -p $1\"" >> $FILETMP
+echo "echo \"#SBATCH -q $2\"" >> $FILETMP
 
+# make it executable
+chmod a+x $FILETMP
+
+# get the submit node address
 REMOTE_HOST=$3
 
-cp $SAMPLE_FILENAME $FILE_TO_TRANSFER
-sed -i "s/${ORIGINAL_PSTRING}/${NEW_PSTRING}/;s/${ORIGINAL_QSTRING}/${NEW_QSTRING}/" $FILE_TO_TRANSFER
-scp -i ~/.ssh/bosco_key.rsa $FILE_TO_TRANSFER $REMOTE_HOST:bosco/glite/bin/
+# default file location
+DESTINATION_FILE="bosco/glite/etc/blahp/slurm_local_submit_attributes.sh"
 
-#scp $FILE_TO_TRANSFER $REMOTE_HOST:
+# check condor version and adjust location of file this does not seem to 
+# be needed for the time being; if a later version of condor changes the
+# location of slurm_local_submit_attributes.sh then use this construct to
+# act appropriately
+#if [[ $MAJOR -ge 9 ]]
+#then
+#  if [[ $MINOR -gt 7 ]]
+#  then
+#      DESTINATION_FILE="bosco/glite/etc/blahp/slurm_local_submit_attributes.sh"
+#  fi
+#fi
+
+# do the transfer, using the bosco_key.rsa
+scp -i ~/.ssh/bosco_key.rsa $FILETMP $REMOTE_HOST:$DESTINATION_FILE
+
+# clean up
+rm $DESTINATION_FILE
+
+# done
