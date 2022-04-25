d1e206b222894581dcf98a09618f52cfee10c5b2
Hasan Baig <hasan.baig@hotmail.com>
2022-04-25 11:28:30 -0400

    initialize and prepare subtask methods are added.

--- web_interface/task_plugins/plugins/profile_likelihood/plugin.py
+++ web_interface/task_plugins/plugins/profile_likelihood/plugin.py
@@ -42,27 +42,26 @@ class TaskForm(BaseTaskForm):
     #Any extra fields for the task submission form
     parameter_estimation_data = forms.FileField(required=True, help_text='Select either a single data file, or if more than one data file is required, upload a .zip file containing multiple data files.')
 
-class TaskPlugin(BaseTask:
+class TaskPlugin(BaseTask):
 
-    subtasks = 2
+    subtasks = 3
 
     def __init__(self, task):
-        self.use_load_balancing = not task.get_custom_field('skip_load_balancing_step')
+        #self.use_load_balancing = not task.get_custom_field('skip_load_balancing_step')
+        self.data_files = task.get_custom_field('data_files')
 
-        if self.use_load_balancing:
-            self.subtasks = 3
-        else:
-            self.subtasks = 2
-            task.set_custom_field('repeats_per_job', 1)
-            task.save()
+        # if self.use_load_balancing:
+        #     self.subtasks = 3
+        # else:
+        #     self.subtasks = 2
+        #     task.set_custom_field('repeats_per_job', 1)
+        #     task.save()
 
         super(TaskPlugin, self).__init__(task)
 
         #added by HB
-        slog.debug("---------> self.directory")
-        slog.debug(self.task.directory)
-        slog.debug("---------> self.task.original_model")
-        slog.debug(self.task.original_model)
+        slog.debug("---------> self.task.directory: {}".format(self.task.directory))
+        slog.debug("---------> self.task.original_model: {}".format(self.task.directory))
 
         slog.debug("+++++++++++ Running BasiCO implementation.")
         self.copasi_model = PLCopasiModel_BasiCO(os.path.join(self.task.directory, self.task.original_model))
@@ -70,5 +69,38 @@ class TaskPlugin(BaseTask:
     def validate(self):
         #TODO:Abstract this to a new COPASI class in this plugin package
         valid_result = self.copasi_model.is_valid('PL')
-        slog.debug(valid_result)
+        slog.debug("Is model valid: {}".format(valid_result))
         return valid_result
+
+    def initialize_subtasks(self):
+        #Create new subtask objects, and save them
+
+        #Create a task to generate PL files for each Parameters
+        self.create_new_subtask('PlFiles', local=True)
+        #Create a task to submit jobs on remote submit node
+        self.create_new_subtask('main')
+
+        #And a subtask to process results locally (on web server)
+        self.create_new_subtask('process', local=True)
+        self.task.save()
+
+    def prepare_subtask(self, index):
+        """Prepare the indexed subtask"""
+        if index == 1:
+            return self.process_plfiles_subtask()
+
+        elif index == 2:
+            return self.process_main_subtask()
+
+        elif index == 3:
+            return self.process_results_subtask()
+
+        else:
+            raise Exception('No subtasks remaining')
+
+
+#READ A QUESTION ON NOTE BOOK
+    def process_plfiles_subtask():
+        subtask = self.get_subtask(1)
+
+        self.copasi_model.prepare_pl_files()
